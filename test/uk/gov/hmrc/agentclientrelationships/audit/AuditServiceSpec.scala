db9272a0ac46d906f72c3563cb8e7189e060747c
rvijayan <rvijayan@users.noreply.github.com>
2017-06-28 09:55:17 +0100

    [APB-918][RV,ZF,AO] Audit check-cesa event

--- test/uk/gov/hmrc/agentclientrelationships/audit/AuditServiceSpec.scala
+++ test/uk/gov/hmrc/agentclientrelationships/audit/AuditServiceSpec.scala
@@ -35,7 +35,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class AuditServiceSpec extends UnitSpec with MockitoSugar with Eventually {
   "auditEvent" should {
-    "send an event with the correct fields" in {
+    "send an CopyRelationship event with the correct fields" in {
       val mockConnector = mock[AuditConnector]
       val service = new AuditService(mockConnector)
 
@@ -85,6 +85,59 @@ class AuditServiceSpec extends UnitSpec with MockitoSugar with Eventually {
         sentEvent.detail("Authorization") shouldBe "dummy bearer token"
 
         sentEvent.tags("transactionName") shouldBe "copy-relationship"
+        sentEvent.tags("path") shouldBe "/path"
+        sentEvent.tags("X-Session-ID") shouldBe "dummy session id"
+        sentEvent.tags("X-Request-ID") shouldBe "dummy request id"
+      }(PatienceConfig(
+        timeout = scaled(Span(500, Millis)),
+        interval = scaled(Span(200, Millis))))
+    }
+
+    "send an CheckCESA event with the correct fields" in {
+      val mockConnector = mock[AuditConnector]
+      val service = new AuditService(mockConnector)
+
+
+      val hc = HeaderCarrier(
+        authorization = Some(Authorization("dummy bearer token")),
+        sessionId = Some(SessionId("dummy session id")),
+        requestId = Some(RequestId("dummy request id"))
+      )
+
+      val auditData = new AuditData()
+      auditData.set("arn",Arn("1234").value)
+      auditData.set("credId", "0000001234567890")
+      auditData.set("agentCode",AgentCode("GG1234567890").value)
+      auditData.set("saAgentRef", "12313")
+      auditData.set("nino",Nino("KS969148D").value)
+      auditData.set("CESARelationship", true)
+
+
+      await(service.sendCheckCESAAuditEvent(
+        hc,
+        FakeRequest("GET", "/path"),
+        auditData)
+      )
+
+      eventually {
+        val captor = ArgumentCaptor.forClass(classOf[AuditEvent])
+        verify(mockConnector).sendEvent(captor.capture())(any[HeaderCarrier], any[ExecutionContext])
+        captor.getValue shouldBe an[DataEvent]
+        val sentEvent = captor.getValue.asInstanceOf[DataEvent]
+
+        sentEvent.auditType shouldBe "CheckCESA"
+        sentEvent.auditSource shouldBe "agent-client-relationships"
+        sentEvent.detail("arn") shouldBe "1234"
+        sentEvent.detail("agentCode") shouldBe "GG1234567890"
+        sentEvent.detail("saAgentRef") shouldBe "12313"
+        sentEvent.detail("credId") shouldBe "0000001234567890"
+        sentEvent.detail("nino") shouldBe "KS969148D"
+        sentEvent.detail("CESARelationship") shouldBe "true"
+
+        sentEvent.tags.contains("Authorization") shouldBe false
+        sentEvent.detail("Authorization") shouldBe "dummy bearer token"
+
+        sentEvent.tags("transactionName") shouldBe "check-cesa"
         sentEvent.tags("path") shouldBe "/path"
         sentEvent.tags("X-Session-ID") shouldBe "dummy session id"
         sentEvent.tags("X-Request-ID") shouldBe "dummy request id"
