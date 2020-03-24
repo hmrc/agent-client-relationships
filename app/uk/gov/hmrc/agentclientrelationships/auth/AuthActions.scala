cc8dbb1f14819b67b08fe902a4cb0a36a4940be0
jordanrowe <jordan.rowe@digital.hmrc.gov.uk>
2020-03-24 16:37:29 +0000

    [APB-4804][JR] Implement Basic Auth for terminations

--- app/uk/gov/hmrc/agentclientrelationships/auth/AuthActions.scala
+++ app/uk/gov/hmrc/agentclientrelationships/auth/AuthActions.scala
@@ -210,16 +210,28 @@ trait AuthActions extends AuthorisedFunctions {
     } catch { case _: Throwable => "" }
 
   def withBasicAuth(expectedAuth: BasicAuthentication)(body: => Future[Result])(
-    implicit request: Request[_]): Future[Result] =
+    implicit request: Request[_]): Future[Result] = {
     request.headers.get(HeaderNames.authorisation) match {
       case Some(basicAuthHeader(encodedAuthHeader)) =>
         decodeFromBase64(encodedAuthHeader) match {
-          case decodedAuth(username, password) if (BasicAuthentication(username, password) == expectedAuth) =>
-            body
-          case _ => Future successful Unauthorized
+          case decodedAuth(username, password) =>
+            if (BasicAuthentication(username, password) == expectedAuth) body
+            else {
+              Logger.warn(
+                "Authorization header found in the request but invalid username or password")
+              Future successful Unauthorized
+            }
+          case _ =>
+            Logger.warn(
+              "Authorization header found in the request but its not in the expected format")
+            Future successful Unauthorized
         }
-      case _ => Future successful Unauthorized
+      case _ =>
+        Logger.warn(
+          "No Authorization header found in the request for agent termination")
+        Future successful Unauthorized
     }
+  }
 
   protected def withAuthorisedAsAgent[A](body: Arn => Future[Result])(
     implicit request: Request[A],
