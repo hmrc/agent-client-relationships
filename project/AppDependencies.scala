62c5505cd4878285c407e1cee69bbe2a5f97cbd3
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-04-11 17:17:26 +0100

    [CS] bump agent-kenshoo-monitoring

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % bootstrapVer,
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"     % "1.2.0",
-    "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"  % "4.8.0-play-28",
+    "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"  % "5.1.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"        % mongoVer,
     "com.kenshoo"        %% "metrics-play"              % "2.7.3_0.8.2",
     "com.github.blemale" %% "scaffeine"                 % "4.0.1",
@@ -19,9 +19,9 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"      % "test, it",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % mongoVer     % "test, it",
     "org.scalatestplus"      %% "mockito-3-12"             % "3.2.10.0"   % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8"             % "2.26.1"     % "test, it",
+    "com.github.tomakehurst" %  "wiremock-jre8"            % "2.26.1"     % "test, it",
     "org.scalamock"          %% "scalamock"                % "4.4.0"      % "test, it",
-    "com.vladsch.flexmark"   % "flexmark-all"              % "0.35.10"    % "test, it",
+    "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"    % "test, it",
     "com.typesafe.akka"      %% "akka-actor-testkit-typed" % "2.6.20"     % "test, it"
   )
 }
