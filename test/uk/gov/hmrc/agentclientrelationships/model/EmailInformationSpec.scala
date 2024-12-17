17e219109dcada881fe607a7352aa2571fd7a6c2
raw1697 <44207483+raw1697@users.noreply.github.com>
2024-12-17 12:39:13 +0000

    APB 8994 -  Email functionality on ACR (#307)
    * created Email Connector
    
    * AppConfig and It/Test implementation
    
    * fixed integration test
    
    ---------
    
    Co-authored-by: vaibhvidhami <vaibhavi.dhami@digital.hmrc.gov.uk>

---
+++ test/uk/gov/hmrc/agentclientrelationships/model/EmailInformationSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentclientrelationships.model
+
+import play.api.libs.json.Json.toJson
+import uk.gov.hmrc.agentclientrelationships.support.UnitSpec
+
+class EmailInformationSpec extends UnitSpec {
+
+  val emails = Seq("someone@something.go.global")
+  val templateId = "client_accepted_email"
+  val parametersAccept = Map("agentName" -> "Agent 1", "clientName" -> "Client 2", "service" -> "Accept ITSA")
+  val parametersExpired =
+    Map(
+      "agentName"  -> "Agent 1",
+      "clientName" -> "Client 2",
+      "service"    -> "Expired ITSA",
+      "expiryDate" -> "1 October 2020"
+    )
+
+  "EmailInformation" should {
+    "return accept email info" in {
+      val emailAcceptInfo = EmailInformation(emails, templateId, parametersAccept, false, None, None)
+
+      val json = toJson(emailAcceptInfo)
+
+      (json \ "to").as[Seq[String]] shouldBe emails
+      (json \ "templateId").as[String] shouldBe templateId
+      (json \ "parameters").as[Map[String, String]] shouldBe parametersAccept
+    }
+
+    "return expired email info" in {
+      val emailExpiredInfo = EmailInformation(emails, templateId, parametersExpired, false, None, None)
+
+      val json = toJson(emailExpiredInfo)
+
+      (json \ "to").as[Seq[String]] shouldBe emails
+      (json \ "templateId").as[String] shouldBe templateId
+      (json \ "parameters").as[Map[String, String]] shouldBe parametersExpired
+    }
+  }
+
+}
