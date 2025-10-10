2394cc105167ed3c0255711560e67b050794b65d
mrolfe88 <52818407+mrolfe88@users.noreply.github.com>
2025-10-10 12:22:09 +0100

    [APB-10089] PR Bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,24 +1,24 @@
 import sbt.*
 
 object AppDependencies {
-  private val mongoVer = "2.7.0"
-  private val bootstrapVer = "10.1.0"
+  private val mongoVer = "2.9.0"
+  private val bootstrapVer = "10.2.0"
   private val pekkoVersion = "1.0.3"
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVer,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % mongoVer,
     "org.typelevel"          %% "cats-core"                 % "2.13.0",
-    "uk.gov.hmrc"            %% "crypto-json-play-30"       % "8.3.0",
+    "uk.gov.hmrc"            %% "crypto-json-play-30"       % "8.4.0",
     "io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.0-pekko-1.0.x",
     "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"    % bootstrapVer % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"        % "7.0.1"      % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"        % "7.0.2"      % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"   % mongoVer     % Test,
     "org.apache.pekko"       %% "pekko-testkit"             % pekkoVersion % Test,
     "org.apache.pekko"       %% "pekko-actor-testkit-typed" % pekkoVersion % Test,
-    "org.scalacheck"         %% "scalacheck"                % "1.17.0"     % Test
+    "org.scalacheck"         %% "scalacheck"                % "1.19.0"     % Test
   )
 }
