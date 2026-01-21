afed4ab96d1eee654e9bbe209a2493061d761a24
kaizar12 <10980326+kaizar12@users.noreply.github.com>
2026-01-21 10:43:27 +0000

    WIP: APB-10280 find duplicate partial auth Ninos (#475)

--- app/uk/gov/hmrc/agentclientrelationships/repository/PartialAuthRepository.scala
+++ app/uk/gov/hmrc/agentclientrelationships/repository/PartialAuthRepository.scala
@@ -215,4 +215,13 @@ with RequestAwareLogging {
       .map(_.getDeletedCount == 1L)
   }
 
+  def getDuplicateAuths(): Future[Int] = collection
+    .find(equal("active", true))
+    .toFuture()
+    .map { records =>
+      records
+        .groupBy(r => (r.arn, r.service, r.nino.take(8)))
+        .count { case (_, rs) => rs.size > 1 }
+    }
+
 }
---
+++ app/uk/gov/hmrc/agentclientrelationships/support/PartialAuthStartupChecks.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentclientrelationships.support
+
+import org.apache.pekko.stream.Materializer
+import play.api.Logging
+import uk.gov.hmrc.agentclientrelationships.repository.PartialAuthRepository
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class PartialAuthStartupChecks @Inject() (
+  partialAuthRepository: PartialAuthRepository
+)(implicit
+  ec: ExecutionContext,
+  mat: Materializer
+)
+extends Logging {
+
+  partialAuthRepository.getDuplicateAuths.map {
+    count =>
+      logger.warn(
+        s"[PartialAuthStartupChecks] duplicate partial auth records: $count"
+      )
+  }
+    .recover {
+      case ex =>
+        logger.warn(
+          "[PartialAuthStartupChecks] Failed to fetch duplicate partial auth records",
+          ex
+        )
+    }
+}
