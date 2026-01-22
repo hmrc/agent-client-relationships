e2b4a5ffed55d90220d69036d6fb19359262ab6f
kaizar12 <10980326+kaizar12@users.noreply.github.com>
2026-01-22 14:19:35 +0000

    APB-10280 rollback duplicate partial auth check (#477)

--- app/uk/gov/hmrc/agentclientrelationships/repository/PartialAuthRepository.scala
+++ app/uk/gov/hmrc/agentclientrelationships/repository/PartialAuthRepository.scala
@@ -215,13 +215,4 @@ with RequestAwareLogging {
       .map(_.getDeletedCount == 1L)
   }
 
-  def getDuplicateAuths(): Future[Int] = collection
-    .find(equal("active", true))
-    .toFuture()
-    .map { records =>
-      records
-        .groupBy(r => (r.arn, r.service, r.nino.take(8)))
-        .count { case (_, rs) => rs.size > 1 }
-    }
-
 }
