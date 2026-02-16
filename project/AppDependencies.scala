89dead6f026ee9a63521c9324ec7fb487a141f32
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-02-16 16:55:27 +0000

    APB-10281: Tidies dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,24 +1,27 @@
 import sbt.*
 
 object AppDependencies {
-  private val mongoVer = "2.10.0"
+
   private val bootstrapVer = "10.3.0"
-  private val pekkoVersion = "1.0.3"
+  private val mongoVer = "2.10.0"
+  private val playVer = "play-30"
+  private val pekkoVer = "1.0.3"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVer,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % mongoVer,
-    "org.typelevel"          %% "cats-core"                 % "2.13.0",
-    "uk.gov.hmrc"            %% "crypto-json-play-30"       % "8.4.0",
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"    % "1.2.0-pekko-1.0.x",
-    "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0"
+    "uk.gov.hmrc"            %% s"bootstrap-backend-$playVer" % bootstrapVer,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-$playVer"        % mongoVer,
+    "uk.gov.hmrc"            %% s"domain-$playVer"            % "11.0.0",
+    "uk.gov.hmrc"            %% s"crypto-json-$playVer"       % "8.4.0",
+    "org.typelevel"          %% "cats-core"                   % "2.13.0",
+    "io.github.samueleresca" %% "pekko-quartz-scheduler"      % "1.2.0-pekko-1.0.x"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"    % bootstrapVer % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"        % "7.0.2"      % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"   % mongoVer     % Test,
-    "org.apache.pekko"       %% "pekko-testkit"             % pekkoVersion % Test,
-    "org.apache.pekko"       %% "pekko-actor-testkit-typed" % pekkoVersion % Test,
-    "org.scalacheck"         %% "scalacheck"                % "1.19.0"     % Test
-  )
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVer"  % bootstrapVer,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVer" % mongoVer,
+    "org.apache.pekko"       %% "pekko-testkit"             % pekkoVer,
+    "org.apache.pekko"       %% "pekko-actor-testkit-typed" % pekkoVer,
+    "org.scalatestplus.play" %% "scalatestplus-play"        % "7.0.2",
+    "org.scalacheck"         %% "scalacheck"                % "1.19.0"
+  ).map(_ % Test)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,11 +3,11 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
-addSbtPlugin("org.playframework"  % "sbt-plugin"          % "3.0.9")
-addSbtPlugin("uk.gov.hmrc"        % "sbt-auto-build"      % "3.24.0")
-addSbtPlugin("uk.gov.hmrc"        % "sbt-distributables"  % "2.6.0")
-addSbtPlugin("org.scalameta"      % "sbt-scalafmt"        % "2.5.4")
-addSbtPlugin("org.scoverage"      % "sbt-scoverage"       % "2.3.1")
+addSbtPlugin("org.playframework"  % "sbt-plugin"             % "3.0.9")
+addSbtPlugin("uk.gov.hmrc"        % "sbt-auto-build"         % "3.24.0")
+addSbtPlugin("uk.gov.hmrc"        % "sbt-distributables"     % "2.6.0")
+addSbtPlugin("org.scalameta"      % "sbt-scalafmt"           % "2.5.4")
+addSbtPlugin("org.scoverage"      % "sbt-scoverage"          % "2.3.1")
 addSbtPlugin("org.scalastyle"     %% "scalastyle-sbt-plugin" % "1.0.0")
 addSbtPlugin("org.wartremover"    %  "sbt-wartremover"       % "3.3.3")
 addSbtPlugin("com.timushev.sbt"   %  "sbt-updates"           % "0.6.4")
