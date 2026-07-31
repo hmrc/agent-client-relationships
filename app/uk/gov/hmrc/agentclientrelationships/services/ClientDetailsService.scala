873e883bb1867fdc94032b2ea66a726e30f4b45f
robomancap <182665093+robomancap@users.noreply.github.com>
2026-07-31 10:56:27 +0100

    Add temp case to cover overseas while feature switch is in place

--- app/uk/gov/hmrc/agentclientrelationships/services/ClientDetailsService.scala
+++ app/uk/gov/hmrc/agentclientrelationships/services/ClientDetailsService.scala
@@ -163,6 +163,9 @@ extends RequestAwareLogging {
                           name = name,
                           factType = Country
                         )))
+                      case (Some(_), Some(_), _, Some(country)) if !isUk(country) =>
+                        // TODO REMOVE THIS CASE WHEN overseasItsaEnabled FEATURE IS ENABLED
+                        Future.successful(Left(ClientDetailsNotFound))
                       case (optName, Some(_), optPostcode, optCountry) if optName.isEmpty || optPostcode.isEmpty || optCountry.isEmpty =>
                         val missingFields =
                           List(
